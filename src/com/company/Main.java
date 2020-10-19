ebbd8fce2de51e68f7806ef6e3ee70a10fc01eb4
Vladyslav-Samokryk <43633541+Vladyslav-Samokryk@users.noreply.github.com>
2020-10-19 20:20:03 +0300

    Updating Server File

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import
+        java.nio.channels.*;
+import java.nio.ByteBuffer;
+import java.net.*;
+import java.util.Set;
+import java.util.Iterator;
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Process server;
+        server = proc.Server.start();
+
+
 }
