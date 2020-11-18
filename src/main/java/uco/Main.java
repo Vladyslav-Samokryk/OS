398b70f015922d216749a3dd1721c92c6b2ffe85
Vladyslav-Samokryk <43633541+Vladyslav-Samokryk@users.noreply.github.com>
2020-11-18 02:19:10 +0200

    Add files via upload

---
+++ src/main/java/uco/Main.java
@@ -0,0 +1,14 @@
+package  uco;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MainManager mainManager = new MainManager();
+
+        while (true) {
+            mainManager.interactiveMenu();
+            mainManager.start();
+        }
+    }
+}
+
+
